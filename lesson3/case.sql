22a759d713d59ea0018c8d9a8a328ad16687a412
laravel.dev <basket.baza@gmail.com>
2022-02-19 17:57:23 +0300

    Add Lesson3: Completed

---
+++ lesson3/case.sql
@@ -0,0 +1,84 @@
+﻿/*********************************************
+*************** -=CASE=- *********************
+*********************************************/
+
+-- -=1=- Простой CASE
+-- если ЗП Ю= 8000 - это руководитель, если => 5000 - менеджер, остальные - рабочие
+select e.FName                                      
+,      e.LName                                      
+,      e.Salary                                     
+,      case when e.salary >=8000  then 'Руководитель' -- если выполянется данное условие - больше ничего не проверяется
+            when e.salary >= 5000 then 'Менеджер'
+            when e.salary is NULL then 'Unknow'
+                                  else 'Рабочий' end SalaryDetail
+,      case when e.salary >=8000  then 'Руководитель'
+            when e.salary >= 5000 then 'Менеджер'
+            when e.salary is NULL then 'Unknow' end  SalaryDetail2
+from Employees e
+order by SalaryDetail
+
+-- -=2=- Поисковый CASE
+select e.id                                                        
+,      e.Department                                                
+,      e.FName                                                     
+,      e.LName                                                     
+,      e.Salary                                                    
+,      case e.Department when 'PRODUCTION & ENGINEERING' then 100
+                         when 'LOGISTICS'                then 25
+                         when 'LAW'                      then 15
+                                                         else 2 end as [Bonus%]
+,      Salary/100*
+case e.Department when 'PRODUCTION & ENGINEERING' then 100
+                  when 'LOGISTICS'                then 25
+                  when 'LAW'                      then 15
+                                                  else 2 end           SalaryBonus
+,      Salary+ Salary/100*
+case e.Department when 'PRODUCTION & ENGINEERING' then 100
+                  when 'LOGISTICS'                then 25
+                  when 'LAW'                      then 15
+                                                  else 2 end           SalaryWithBonus
+from Employees e
+order by [bonus%]
+,        Department
+,        Salary
+
+-- -=3=-  IIF
+select e.FName                                      
+,      e.LName                                      
+,      e.Salary  
+,  IIF(e.Salary>=6000,'manager', 'worker') as position,
+  case  when e.Salary>=6000 then 'manager' else 'worker' end position_case
+from Employees e
+
+-- добавим пол в таблицу employee
+alter table Employees add gender bit
+-- Обновление на основе IIF
+update Employees set gender= IIF(id<50,0,1)
+
+-- вывод пола
+select e.FName                                      
+,      e.LName                                      
+,      e.Salary 
+, IIF(e.gender=0,'ж','м' )
+from Employees e
+
+---=3=- GROUP BY 
+select e.Department,IIF(e.gender=0,'ж','м' ) as  gender,count( e.gender) cnt
+from Employees e
+group by e.Department, e.gender
+order by 1,2,3
+
+-- подразделение включающее символ L и с количеством м или ж > 3
+select e.Department            
+,      IIF(e.gender=0,'ж','м' ) as gender
+,      count( e.gender)            cnt
+from Employees e
+group by e.Department
+,        e.gender
+having count( e.gender)>3
+	and e.Department like '%L%'
+order by 1
+,        2
+,        3
+
+
---
+++ lesson3/where.sql
@@ -0,0 +1,79 @@
+﻿/*********************************************
+**************-=WHERE=- *********************
+*********************************************/
+Use BasDB;
+-- ЗП =10000
+SELECT   e.*
+FROM  employees e
+WHERE e.Salary=10000
+GO
+-- все сотрудники sales
+SELECT  e.*
+FROM  employees e
+WHERE e.Department='sales'
+
+-- дата  рождения не больше  10.09.1986
+select *
+from Employees e
+where e.BirthDate !> '19860910'
+
+-- подразделение sail и ЗП не менее 10000
+select *
+from Employees e 
+where e.Department='sales' and e.Salary!<10000
+
+-- подразделение sail или supply c ЗП>6000
+select *
+from Employees e
+where (e.Department ='sales' or e.Department='supply') and (e.Salary>6000 or e.Salary is NULL)
+order by e.Department, e.Salary
+ -- все сотрудники подразделеий 'sales','supply','law', 'logistic'
+select *
+from Employees e
+where (e.Department in ('sales','supply','law', 'logistic')) and (e.Salary>6000 or e.Salary is NULL)
+order by e.Department, e.Salary
+-- сотрудники не больше моего дня рождения но не больше 1986.01.01
+select *
+from Employees e
+--where e.BirthDate!<'19860101' and e.BirthDate !> '19860910' 
+--or 
+where e.BirthDate between '19860101'and  '19860910' 
+
+--=7=- LIKE 
+-- сотрудники с номером телефона на 063
+select *
+from Employees e
+where e.Phone like '063%'
+
+-- вторая цифра в ID =2 
+select *
+from Employees e
+where e.id like '_2'
+--первая цифра 2 или 4 вторая цифра в ID=3 c учетом 3
+select *
+from Employees e
+where e.Id=3 or  e.id like '[2,4]3'
+--первая цифра с 2 по 5 вторая цифра в ID=3 c учетом 3
+select *
+from Employees e
+where e.Id=3 or  e.id like '[2-5]3'
+ --  первая цифра не должны быть с 2 по 5 вторая цифра в ID=3  
+select *
+from Employees e 
+where   e.id like '[^2-5]3'
+ 
+
+ --8-- NULL
+ --все зп с NULL
+select *
+from Employees e
+where e.Salary is null
+-- значение с NULL не попадут в выборку
+select *
+from Employees e
+where e.Salary in (4000,7000,null)
+
+-- значение с NULL попадут в выборку
+select *
+from Employees e
+where e.Salary in (4000,7000) or e.Salary is null
